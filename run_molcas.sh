01ffc2057eeb8847e347b64d3b88b6c3e6728353
vamshimohank <vamshimohank@gmail.com>
2019-05-19 14:11:23 +0200

    cleaned up paramiko_helper

--- run_molcas.sh
+++
@@ -1,8 +0,0 @@
-#. /home/katukuri/Codes/MOLCAS/molcas_config.sh
-#export REMOTE_MACHINE_IP=allogin2.fkf.mpg.de
-#export REMOTE_NECI_WorkDir=/algpfs/katukuri/molcas_neci
-#export MOLCAS_
-#export CurrDir=$PWD
-export WorkDir=$PWD/tmp
-. /home/katukuri/Codes/MOLCAS/molcas_mod_config.sh
-pymolcas -new -f -b1 $1 &
